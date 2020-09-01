50ece436235b1dd1427e848c004cfc65e45f4881
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-09-01 20:31:27 -0300

    Dicionário v.1

---
+++ dicionario.py
@@ -0,0 +1,6 @@
+lista_telefonica = {}
+lista_telefonica['Orlando'] = ['Orlando Saraiva','Rio Claro']
+lista_telefonica['marcelo'] = ['Marcelo','Araras']
+lista_telefonica['andre'] = ['André','Araras']
+lista_telefonica['xyz'] = 'Teste'
+lista_telefonica['xy'] = 15
