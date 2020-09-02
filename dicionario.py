5fd53a8a26a0b3507acedf59a53a2788570efd11
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-09-01 21:40:52 -0300

    Dicionário v.2 + sets + funções COM Testes

--- dicionario.py
+++ dicionario.py
@@ -4,3 +4,20 @@ lista_telefonica['marcelo'] = ['Marcelo','Araras']
 lista_telefonica['andre'] = ['André','Araras']
 lista_telefonica['xyz'] = 'Teste'
 lista_telefonica['xy'] = 15
+
+try:
+    print(lista_telefonica['orlando'])
+except KeyError:
+    print(0)
+
+try:
+    print(lista_telefonica['Orlando'])
+except KeyError:
+    print(0)
+
+
+print(lista_telefonica.get('Orlando',0))
+print(lista_telefonica.get('orlando',0))
+
+print('marcelo' in list(lista_telefonica))
+print('Marcelo' in list(lista_telefonica))
---
+++ funcoes.py
@@ -0,0 +1,11 @@
+def soma(num1, num2):
+    return num1 + num2
+
+assert( soma(1,2) == 3)
+assert( soma('oi','mundo') == 'oimundo')
+assert( soma(5.6, 6.2) == 11.8)
+a = [1,2,3,4]
+b = [5,6,7,8]
+c = [1,2,3,4,5,6,7,8]
+assert( soma(a, b) == c)
+       
