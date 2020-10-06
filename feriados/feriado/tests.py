0600f01d1e20979811e2cf223fd90d0898a8ff88
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-10-06 20:10:51 -0300

    Adicionado feriado de carnaval

--- feriados/feriado/tests.py
+++ feriados/feriado/tests.py
@@ -13,3 +13,17 @@ class NatalTest(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+
+class CarnavalTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/carnaval')
+
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'carnaval')
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'carnaval.html')  
--- feriados/feriado/views.py
+++ feriados/feriado/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 
 def natal(request):
     contexto = {'natal': False,
     'carnaval': False}
     return render(request, 'natal.html', contexto)
+
+def carnaval(request):
+    contexto = {'carnaval': False}
+    return render(request, 'carnaval.html', contexto)
